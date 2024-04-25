a40ee08c2437ecee9af9425e3ddbaa682e580972
iamvedantp <vedantp183@gmail.com>
2024-04-25 21:28:23 +0530

    Otp backend issue resolved

--- lib/home.dart
+++ lib/home.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:otp_login_page/login_page.dart'; // Assuming login.dart is imported correctly
 
 class MyHome extends StatefulWidget {
-  const MyHome({super.key});
+  const MyHome({Key? key}) : super(key: key);
 
   @override
   State<MyHome> createState() => MyHomeState();
 }
 
 class MyHomeState extends State<MyHome> {
+  void _logout() {
+    // Navigate back to the login screen
+    Navigator.pushReplacementNamed(context, 'login');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Welcome')
+        title: const Text('Welcome'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: _logout,
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome to the Home Page!',
+              style: TextStyle(fontSize: 20),
+            ),
+            // Add more widgets here for the home screen
+          ],
+        ),
       ),
     );
   }
